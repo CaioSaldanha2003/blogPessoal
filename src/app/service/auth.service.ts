9111e06503e630da297ff570647d4c4457e7f306
Caio Saldanha <caiosaldanha2231@gmail.com>
2021-10-06 20:59:17 -0300

    Implementação do post e get de postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -22,6 +22,10 @@ export class AuthService {
     return this.http.post<User>('https://generationblogpessoalsaldanha.herokuapp.com/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://generationblogpessoalsaldanha.herokuapp.com/usuarios/${id}`)
+  }
+
   logado(){
     let ok: boolean = false
 
